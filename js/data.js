9e432d6e004e08e1ccd60313545b3cc7a81047dd
DiegoAndresGarciaGuacci <dagarciaguacci1@gmail.com>
2023-08-22 10:38:14 -0300

    fin
    si fin

---
+++ js/data.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dataSpan = document.getElementById("data");
+  
+    const storedData = localStorage.getItem("userData");
+  
+    if (storedData) {
+      dataSpan.textContent = storedData;
+    } else {
+      dataSpan.textContent = "No hay datos almacenados";
+    }
+  });
